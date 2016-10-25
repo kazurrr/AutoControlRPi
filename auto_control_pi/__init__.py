b9af1d49be6865498e294d81ee63b607d9c7663a
kazurrr <Ka43ro21>
2016-10-25 19:16:07 +0200

    Prepare repo

---
+++ auto_control_pi/__init__.py
@@ -0,0 +1,7 @@
+from auto_control_pi import metadata
+
+
+__version__ = metadata.version
+__author__ = metadata.authors[0]
+__license__ = metadata.license
+__copyright__ = metadata.copyright
---
+++ auto_control_pi/metadata.py
@@ -0,0 +1,15 @@
+"""Project metadata
+Information describing the project.
+"""
+
+package = 'auto_control_pi'
+project = "AutoControl RPi"
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = ''
+authors = ['Karol Masuhr']
+authors_string = ', '.join(authors)
+emails = ['karol@masuhr.pl']
+license = ''
+copyright = '2016 ' + authors_string
+url = 'https://github.com/kazurrr/AutoControlRPi'
